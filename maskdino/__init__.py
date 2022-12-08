578d8109d92fcdf5f8c9b96472b0631339c4da6d
FengLi-ust <fliay@connect.ust.hk>
2022-12-08 09:59:01 +0800

    Update __init__.py

--- maskdino/__init__.py
+++ maskdino/__init__.py
@@ -14,12 +14,7 @@ from .config import add_maskformer2_config
 from .data.dataset_mappers.coco_instance_new_baseline_dataset_mapper import COCOInstanceNewBaselineDatasetMapper
 from .data.dataset_mappers.coco_panoptic_new_baseline_dataset_mapper import COCOPanopticNewBaselineDatasetMapper
 from .data.dataset_mappers.detr_dataset_mapper import DetrDatasetMapper
-from .data.dataset_mappers.mask_former_instance_dataset_mapper import (
-    MaskFormerInstanceDatasetMapper,
-)
-from .data.dataset_mappers.mask_former_panoptic_dataset_mapper import (
-    MaskFormerPanopticDatasetMapper,
-)
+
 from .data.dataset_mappers.mask_former_semantic_dataset_mapper import (
     MaskFormerSemanticDatasetMapper,
 )
